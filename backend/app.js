094586f4a6f8d28f421dcd18ee296642273a3685
Dan Iozsa <diozsa@yahoo.com>
2023-02-19 21:55:23 -0700

    added routes and their tests

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const { NotFoundError } = require("./expressError");
 const { authenticateJWT } = require("./middleware/auth");
 const authRoutes = require("./routes/auth");
 const usersRoutes = require("./routes/users");
-//const locationsRoutes = require("./routes/locations");
+const addressesRoutes = require("./routes/addresses");
 
 const app = express();
 
@@ -21,7 +21,7 @@ app.use(authenticateJWT);
 
 app.use("/auth", authRoutes);
 app.use("/users", usersRoutes);
-//app.use("/addresses", addressesRoutes);
+app.use("/addresses", addressesRoutes);
 
 
 /** Handle 404 errors -- this matches everything */
---
+++ backend/weather-seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (username, password)
+VALUES
+('user', '$2b$12$w38HHZmTTuTrUKwjkNAIb.L/BRceURuYaH6hYnc53rDHzMlZZW5bC'),
+('user1', '$2b$12$qCHaADpsBw8TTWNifisLL./Bq7JtTaBhMMuvtaxKAZ1ee4pg/sBPi');
+
+INSERT INTO addresses ()
